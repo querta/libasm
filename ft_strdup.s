71894635d7bb06337ab58b40b3a80a247a80b07b
Malik Monte <mmonte@mi-k3.msk.21-school.ru>
2021-03-19 13:39:42 +0300

    all done

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,22 +6,23 @@ section .text
 	global _ft_strdup
 
 _ft_strdup:
-		push	rdi			; Сохраняю 0аргумент в стек
-		call	_ft_strlen	; принимает в rdi строку, возвращает в rax размер
-		inc	rax				; для \0
-		mov	rdi, rax		; Записываем размер строки в rdi
-		call	_malloc		; Возвращает в rax указательно на новую строку
-		jz	error			; или 0
-		pop	rdi				
-		mov	rsi, rdi		; Строку из стека во 2 аргумент
-		mov	rdi, rax		; указатель от маллока в 1
-		call	_ft_strcpy
-		ret
+	push	rdi			; Сохраняю 0аргумент в стек
+	call	_ft_strlen	; принимает в rdi строку, возвращает в rax размер
+	inc	rax				; для \0
+	mov	rdi, rax		; Записываем размер строки в rdi
+	call	_malloc		; Возвращает в rax указательно на новую строку
+	jz	error			; или 0
+	pop	rdi				
+	mov	rsi, rdi		; Строку из стека во 2 аргумент
+	mov	rdi, rax		; указатель от маллока в 1
+	call	_ft_strcpy
+	ret
 
 error:
+	push r8
+	mov r8, rax
 	call ___error
-	mov rdi, 12
-	mov [rax], rdi
-	pop rdi
+	mov [rax], r8
+	pop r8
 	mov rax, 0
 	ret
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmonte <mmonte@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 17:37:18 by mmonte            #+#    #+#             */
-/*   Updated: 2021/03/18 17:26:59 by mmonte           ###   ########.fr       */
+/*   Updated: 2021/03/19 13:38:38 by mmonte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,59 @@
 
 int main()
 {
-	char *src = "abcdefjh";
-	char dst[9];	
+	char *empty = "";
+	char *strlong = "asdfghjklzxcvbnmqwertyuiop[];0asdfghjklzxcvbnmqwertyuiop[]0asdfghjklzxcvbnmqwertyuiop[];012345657890";
+	char *str = "prosto stroka";
+	char dstlong[101];
+	char dstlong_orig[101];
+	char dst[14];
+	char dst_orig[14];
+	char dstempty[5];
+	char dstempty_orig[5];
 
-	printf("ft_strlen %zu", ft_strlen("abcd12"));
-	printf("\nft_strcpy=%s", ft_strcpy(dst, src));
-	
-	char *str1 = "йййй"; // й - 208
-	char *str2 = "ааааа";
-	char *str3 = "zzzz";
+	printf("--=== ft_strlen ===---\n");
+	printf("empty\t orig:%zu my:%zu\nlong\t orig:%zu my:%zu\nregular\t orig:%zu my:%zu\n",
+		strlen(empty), ft_strlen(empty), strlen(strlong), ft_strlen(strlong),
+		strlen(str), ft_strlen(str));
+		
+	printf("--=== ft_strcpy ===---\n");
+	printf("empty\n\torig:\t%s \n\tmy:\t%s \nlong\n\torig:\t%s \n\tmy:\t%s \nregular\n\torig:\t%s\n\tmy:\t%s\n", 
+		strcpy(dstempty_orig, empty), ft_strcpy(dstempty, empty), 
+		strcpy(dstlong_orig, strlong), ft_strcpy(dstlong, strlong),
+		strcpy(dst_orig, str), ft_strcpy(dst, str));
+
+	printf("--=== ft_strcmp ===---\n");
+
+	char *str1 = ""; 
+	char *str2 = "";
+	char *str3 = "Ййаы";
 	char *str4 = "aaaa";
 	char *nums1 = "1234";
 	char *nums2 = "4321";
-	printf("\n");
-	printf("\nft_strcmp: %d:%d, %d:%d, %d:%d", strcmp(str1, str2), ft_strcmp(str1, str2), strcmp(nums1, nums2), ft_strcmp(nums1, nums2), strcmp(nums2, nums1), ft_strcmp(nums2, nums1));
-	printf("\ncmptest\n %s:%s %d:%d \n %s:%s %d:%d \n %s:%s %d:%d \n %s:%s %d:%d \n %s:%s %d:%d \n %s:%s %d:%d \n",
+	printf("\e[1;37m\n str1:str2\torig:my\n\e[0m");
+	printf(" %s:%s \t%d:%d \n %s:%s \t%d:%d \n %s:%s \t%d:%d \n %s:%s \t%d:%d \n %s:%s \t%d:%d \n %s:%s \t%d:%d \n",
 		str1, str2, strcmp(str1, str2), ft_strcmp(str1, str2),
-		str1, nums1, strcmp(str1, nums1), ft_strcmp(str1, nums1),
-		nums2, str2, strcmp(nums2, str2), ft_strcmp(nums2, str2),
-		str3, str4, strcmp(str3, str4), ft_strcmp(str3, str4),
+		str1, str3, strcmp(str1, str3), ft_strcmp(str1, str3),
+		str3, str1, strcmp(str3, str1), ft_strcmp(str3, str1),
+		str4, str4, strcmp(str4, str4), ft_strcmp(str4, str4),
 		nums1, str3, strcmp(nums1, str3), ft_strcmp(nums1, str3),
 		nums1, nums2, strcmp(nums1, nums2), ft_strcmp(nums1, nums2)
 		);
 	printf("\n");
-	// ssize_t		ft_write(int fildes, const void *buf, size_t nbyte);
-	// int fd;
-	// int fd2;
-	// char *buf = "slovo";
-	// int written;
-	// int written2;
+	printf("--=== ft_write ===---\n");
 
-
-	// // fd = open("./file2", O_RDWR);
-	// // fd = open("./file", O_CREAT | O_RDONLY);
-	// fd = open("./file.txt", O_CREAT | O_RDWR, 416);
-	// fd2 = open("./file2.txt", O_CREAT | O_RDWR, 416);
-	// // if (fd == -1)
-	// // {
-	// // 	printf("error\n: %d\n", errno);
-	// // 	return (0);
-	// // }
-
-	// written = ft_write(fd, buf, 4);
-	// printf("\n WRITE:\n%u байтов записано в файл\n", written);
-	// printf("error: %d\n", errno);
-	// written2 = write(fd2, buf, 4);
-	// printf("\n WRITE:\n%u байтов записано в файл\n", written2);
-	// printf("orig error: %d\n", errno);
-
-	// printf("\n \n READ\n");
-	// close(fd);
-	// close(fd2);
-
-	// fd = open("./file.txt", O_RDONLY);
-	// fd2 = open("./file2.txt", O_RDONLY);
 	int fd;
 	int written;
-	char *buf = "slovo";
+	int written_or;
+	char *buf = "slovo123";
 
+	printf("\tstdout\n");
+	written = ft_write(1, buf, 7);
+	written_or = write(1, buf, 7);
+	printf("\norig\tнапечатано:%d, errno:%d\n", written, errno);
+	printf("my\tнапечатано:%d, errno:%d\n", written, errno);
+
+	printf("\topen descriptor\n");
 	fd = open("./file.txt", O_CREAT | O_RDWR, 416);
 	written = write(fd, buf, 4);
 	printf("orig\tзаписано:%d, errno:%d\n", written, errno);
@@ -82,24 +77,56 @@ int main()
 	printf("my\tзаписано:%d, errno:%d\n", written, errno);
 	close(fd);
 
+	printf("\twrong fd\n");
+	fd = open("./file_not_exist", O_RDWR);
+	written = write(fd, buf, 4);
+	printf("orig\tзаписано:%d, errno:%d\n", written, errno);
+	close(fd);
+	errno = 0;
+	
+	fd = open("./file_not_exist", O_RDWR);
+	written = ft_write(fd, buf, 4);
+	printf("my\tзаписано:%d, errno:%d\n", written, errno);
+	close(fd);
+	errno = 0;
 
-
-	char text[4];
+	printf("--=== ft_read ===---\n");
+	char text[10];
 	int readed;
 
+	printf("\tВведи 5 символов:\n");
+	readed = read(0, text, 5);
+	printf("\n text:%s, прочитано:%d, errno:%d\n", text, readed, errno);
+
+	printf("\n");
+	printf("\topen descriptor\n");
 	fd = open("./file.txt", O_RDONLY);
 	readed = read(fd, text, 2);
-	printf("orig\ttext:%s, rb:%d, errno:%d\n", text, readed, errno);
+	printf("orig\ttext:%s, прочитано:%d, errno:%d\n", text, readed, errno);
 	close(fd);
 
 	fd = open("./file.txt", O_RDONLY);
 	readed = ft_read(fd, text, 2);
-	printf("my\ttext:%s, rb:%d, errno:%d\n", text, readed, errno);
+	printf("my\ttext:%s, прочитано:%d, errno:%d\n", text, readed, errno);
 	close (fd);
 
-	char *strdupsrc = "aaaaaaa";
-	char *strdupdst = ft_strdup(strdupsrc);
-	printf("%s", strdupdst);
-	printf("|\n");
+	printf("\twrong fd\n");
+	fd = open("./file_not_exist", O_RDWR);
+	readed = read(fd, text, 2);
+	printf("orig\ttext:%s, прочитано:%d, errno:%d\n", text, readed, errno);
+	close(fd);
+	errno = 0;
+
+	fd = open("./file_not_exist", O_RDWR);
+	readed = ft_read(fd, text, 2);
+	printf("my\ttext:%s, прочитано:%d, errno:%d\n", text, readed, errno);
+	close (fd);
+	errno = 0;
+
+	printf("--=== ft_strdup ===---\n");
+	printf("empty\n\torig:\t%s \n\tmy:\t%s \nlong\n\torig:\t%s \n\tmy:\t%s \nregular\n\torig:\t%s\n\tmy:\t%s\n", 
+	strdup(empty), ft_strdup(empty), 
+	strdup(strlong), ft_strdup(strlong),
+	strdup(str), ft_strdup(str));
 	return (0);
 }
